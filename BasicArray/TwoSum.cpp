158a11a31d1bd4fbd806c3edf8ab37d5caa115a1
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-05-17 00:08:33 +0530

    two sum question solved with 2 approach

---
+++ BasicArray/TwoSum.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map> 
+
+using namespace std ;
+
+//brute force
+vector<int> twoSum(vector<int>& nums,int target){
+    int n=nums.size() ;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]+nums[j]==target){
+                return {i,j} ;
+            }
+        }
+    }
+
+    return {} ;// no solution found
+}
+
+
+//hashmap
+
+vector<int> twoSum(vector<int>& nums,int target){
+    unordered_map<int,int> mp ;
+    int n=nums.size() ;
+
+    for(int i=0;i<n;i++){
+        mp[nums[i]]++;
+    }
+    // for(auto it:nums){
+    //     mp[it]++ ;
+    // }
+
+    //find the complement 
+    for(int i=0;i<n;i++){
+        int compliment=target-nums[i];
+        if(mp.count(compliment) && mp[compliment]!=i){
+            return {i,mp[compliment]} ;
+        }
+    }
+    return {} ;// no solution found 
+}
