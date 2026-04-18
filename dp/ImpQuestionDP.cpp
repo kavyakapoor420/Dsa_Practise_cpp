304784946c61aaa65332b83df5afb35da0b89c22
Kavyakapoor <167117026+kavyakapoor420@users.noreply.github.com>
2026-04-18 21:36:29 +0530

    Add minDistance and maxProfit functions
    Implemented functions for calculating minimum edit distance and maximum profit from stock prices.

--- dp/ImpQuestionDP.cpp
+++ dp/ImpQuestionDP.cpp
@@ -53,3 +53,95 @@ public:
         return dp[m][n];
     }
 };
+
+
+
+// edit distance  Minimum indertion or deletion or replace charcater to conert string1 to string2 
+
+int minDistance(string s,string t){
+
+    int n=s.size() ;
+    int m=t.size() ;
+
+    for(int i=0;i<=m;i++) dp[i][0]=i;
+    for(int j=0;j<=n;j++) dp[0][j]=j;
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            
+            if(s[i-1]==t[j-1]){
+                dp[i][j]=0+dp[i-1][j-1];
+            }else{
+                dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]}) ; // insert update delet character 
+            }
+        }
+    }
+    return dp[n][m];
+
+    // to print LCS first build up Dp table then backtrcaking for get LCS 
+    int i=n;
+    int j=m;
+    string lcs="" ;
+
+    while(i>0 && j>0){
+        if(s[i-1]==t[j-1]){
+            lcs.push_back(s[i-1]);
+            i--;
+            j--;
+        }else if(dp[i-1][j]>dp[i][j-1]){
+            i--;
+        }else{
+            j--;
+        }
+    }
+    reverse(lcs.begin(),lcs.end()) ;
+    return lcs; 
+
+}
+
+
+// buy sell stock 
+
+
+int maxProfit(vector<int>& prices) {
+        int n = prices.size(), maxProfit = 0;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(prices[j] - prices[i] > maxProfit){
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+        return maxProfit;
+   }
+   
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+};
